941af8bb79f3517a0de6195cbe5c50431055267c
KITTY\Catherine <cat@ovsy.com>
2021-02-02 18:22:36 -0800

    Jumping and movement finished

---
+++ space axolotl/Assets/Scripts/NewPlayerControl.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewPlayerControl : MonoBehaviour
+{
+     public InputActionReference movementControl;
+    public InputActionReference jumpControl;
+    public CharacterController controller;
+    
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    Transform cameraMainTransform;
+
+    public float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+
+
+    private float playerSpeed = 6.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+    public float rotationSpeed = 4f;
+
+    private void OnEnable()
+    {
+        movementControl.action.Enable();
+        jumpControl.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movementControl.action.Disable();
+        jumpControl.action.Disable();
+    }
+
+    private void Start()
+    {
+       // controller = gameObject.AddComponent<CharacterController>();
+        cameraMainTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = movementControl.action.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        move = cameraMainTransform.forward *move.z + cameraMainTransform.right * move.x;
+        move.y = 0;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (jumpControl.action.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if (movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cameraMainTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+
+    }
+}
+
--- space axolotl/Assets/Scripts/ThirdPersonMovement.cs
+++ space axolotl/Assets/Scripts/ThirdPersonMovement.cs
@@ -8,6 +8,9 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
+    float originalHeight;
+    public float crouchHeight;
+
     private float gravity = 9.81f;
     private float jumpSpeed = 3.5f;
     private float doubleJumpMultiplier = 0.5f;
@@ -23,6 +26,12 @@ public class ThirdPersonMovement : MonoBehaviour
   //  public Rigidbody rb;
   //  public bool cubeIsOnGround = true;
 
+
+    void start()
+    {
+        originalHeight = controller.height;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -37,40 +46,59 @@ public class ThirdPersonMovement : MonoBehaviour
 
             if (Input.GetButtonDown("Jump"))
             {
-                directionY = jumpSpeed;
-
-                
+               directionY = jumpSpeed;
             }
         } else 
             {
                 if(Input.GetButtonDown("Jump") && canDoubleJump) 
                 {
-                     directionY = jumpSpeed * doubleJumpMultiplier;
+                    directionY = jumpSpeed * doubleJumpMultiplier;
+
                      canDoubleJump = false;
                 }
                 
             }
 
-         if (direction.magnitude >=0.1f && !controller.isGrounded)
+         if (direction.magnitude >=0.1f || Input.GetButtonDown("Jump")) 
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform .rotation = Quaternion.Euler(0f, angle,0f); 
+
+            direction.y -= gravity * Time.deltaTime;
+
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            directionY -= gravity * Time.deltaTime;
-            direction.y = directionY;
+           
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-       else 
-        {
-            //directionY -= gravity * Time.deltaTime;
-            //direction.y = directionY;
-            //controller.Move(direction * speed * Time.deltaTime);
-        }
+        
         
 
+
+
         Debug.Log(direction.magnitude);
+
+
+        
+         if(Input.GetKeyDown(KeyCode.LeftShift))
+         {
+             Crouch();
+         }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+         {
+            goUp();
+         }
+
     }
 
+    void Crouch()
+        {
+            controller.height = crouchHeight;
+        }
+
+    void goUp()
+        {
+            controller.height = originalHeight;
+        }
     
 }

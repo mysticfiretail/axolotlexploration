28fb5090b96f5219c9310b83ee2811ae02806d3b
KITTY\Catherine <cat@ovsy.com>
2021-02-09 19:08:37 -0800

    Movement and animations added

--- space axolotl/Assets/Scripts/NewPlayerControl.cs
+++ space axolotl/Assets/Scripts/NewPlayerControl.cs
@@ -9,8 +9,7 @@ public class NewPlayerControl : MonoBehaviour
     public InputActionReference jumpControl;
     public InputActionReference crouchControl;
     public CharacterController controller;
-    public BoxCollider head;
-    public BoxCollider body;
+    PlayerControls Input;
     
     private Vector3 playerVelocity;
     private bool groundedPlayer;
@@ -20,15 +19,25 @@ public class NewPlayerControl : MonoBehaviour
 
     float turnSmoothVelocity;
 
-
+    public Animator animator;
     private float playerSpeed = 6.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
     public float rotationSpeed = 4f;
     float originalHeight;
     public float crouchHeight;
+    public bool crouchPressed;
+    private bool isWalking = false;
+    private bool isCrouching = false;
+    private bool isJumping = false;
+    private float animationFinishTime = 0.9f;
 
 
+    void Awake()
+    {
+       Input = new PlayerControls();
+       Input.Player.Crouch.performed += ctx=> crouchPressed = ctx.ReadValueAsButton();
+    }
 
     private void OnEnable()
     {
@@ -48,10 +57,14 @@ public class NewPlayerControl : MonoBehaviour
     {
        // controller = gameObject.AddComponent<CharacterController>();
         cameraMainTransform = Camera.main.transform;
+        originalHeight = controller.height; 
     }
 
     void Update()
     {
+
+        
+
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
@@ -67,6 +80,7 @@ public class NewPlayerControl : MonoBehaviour
         // Changes the height position of the player..
         if (jumpControl.action.triggered && groundedPlayer)
         {
+            AnimateJump();
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
@@ -80,28 +94,88 @@ public class NewPlayerControl : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
         }
 
+        
+        AnimateWalk(movement);
+       
 
-        if(crouchControl.action.triggered)
+
+
+
+   if (crouchControl.action.triggered)
          {
-             Crouch();
-         }
-        else
-         {
-            goUp();
+        if(controller.height == crouchHeight)
+            {
+                animator.ResetTrigger("isCrouching");
+                controller.center = new Vector3(0, 0.99f, 0);
+                controller.height = originalHeight;
+            }
+            else 
+            {
+                controller.height = crouchHeight;
+                controller.center = new Vector3(0, 0.46f, 0);
+                animator.SetTrigger("isCrouching");
+            }
+
          }
 
+ 
+
+      /*  if(crouchPressed == true)
+         {
+             controller.height = crouchHeight;
+             
+             Debug.Log("crouch");
+         }
+        else if(!crouchPressed)
+        {
+            controller.height = originalHeight;
+          //  Debug.Log("up");
+        } 
+*/
+    
+    if(isCrouching && animator.GetCurrentAnimatorStateInfo(1).normalizedTime >= animationFinishTime)
+    {
+        isCrouching = false;
+    }
+
+    }
+    
+
+    void AnimateWalk(Vector3 movement)
+    {
+        isWalking = (movement.x >0.1f || movement.x < -0.1) || (movement.y >0.1f || movement.y < -0.1) ? true : false;
+        animator.SetBool("isWalking", isWalking);
+    }
+
+    void AnimateJump()
+    {
+        animator.SetBool("isJumping", isJumping);
+    }
+
+    void Crouching ()
+    {
+        if(!isCrouching)
+        {
+            animator.SetTrigger("isCrouching");
+            StartCoroutine(InitialiseCrouch());
+        }
+    }
+
+    IEnumerator InitialiseCrouch ()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isWalking = true; 
     }
 
     void Crouch()
         {
-
+            controller.height = crouchHeight;
         }
 
     void goUp()
         {
- 
+            controller.height = originalHeight;
         }
 
-    
 }
 
---
+++ space axolotl/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,263 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""25df7bb2-94ad-4d07-a77e-2f9b8bf53cfb"",
+            ""actions"": [
+                {
+                    ""name"": ""MouseLook"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""b465702e-a2b1-4cd1-999b-953a3d5aa122"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""6d64909c-d657-4775-8f25-81f91fd447a4"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""38aac90e-4b45-413b-8668-7428f3814182"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Crouch"",
+                    ""type"": ""Button"",
+                    ""id"": ""d55873af-428f-4d96-be2b-94973a2cfc63"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""8c75318f-726d-413b-9e40-98574f142d08"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MouseLook"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""1302572d-a5ec-4f6b-85b5-a5336d50aa14"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2779e973-41a6-48ac-9e2f-42e813bd3658"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""5a2ed868-52b1-4f73-8623-9c994833ee49"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""7a0c75e4-9472-4597-a231-28fd77fe2439"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""223df4a9-08a7-4353-93d5-a6155a8b1ed0"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b7c51547-ecc0-4b20-81bf-e39c7d327e51"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""164a8272-5f23-4d2d-b08e-409862db9faa"",
+                    ""path"": ""<Keyboard>/leftShift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_MouseLook = m_Player.FindAction("MouseLook", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Crouch = m_Player.FindAction("Crouch", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_MouseLook;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Crouch;
+    public struct PlayerActions
+    {
+        private @PlayerControls m_Wrapper;
+        public PlayerActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @MouseLook => m_Wrapper.m_Player_MouseLook;
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Crouch => m_Wrapper.m_Player_Crouch;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @MouseLook.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMouseLook;
+                @MouseLook.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMouseLook;
+                @MouseLook.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMouseLook;
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Crouch.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCrouch;
+                @Crouch.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCrouch;
+                @Crouch.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnCrouch;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @MouseLook.started += instance.OnMouseLook;
+                @MouseLook.performed += instance.OnMouseLook;
+                @MouseLook.canceled += instance.OnMouseLook;
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Crouch.started += instance.OnCrouch;
+                @Crouch.performed += instance.OnCrouch;
+                @Crouch.canceled += instance.OnCrouch;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMouseLook(InputAction.CallbackContext context);
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnCrouch(InputAction.CallbackContext context);
+    }
+}

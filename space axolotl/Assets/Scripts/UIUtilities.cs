771ce2def5a743c3546f7188932e5f730e5022bf
DESKTOP-QOLC2A6\Caitlin <c.arnold116@gmail.com>
2021-05-15 23:43:21 -0700

    Didn't break it, Cat fixed it

--- space axolotl/Assets/Scripts/UIUtilities.cs
+++ space axolotl/Assets/Scripts/UIUtilities.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class UIUtilities : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject BBoop;
     public void LoadScene(string SceneName)
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneName);
     }
 
+    public void RestartCheckpoint()
+    {
+        player.transform.position = new Vector3(player.GetComponent<CheckpointSystem>().currentCheckpoint.transform.position.x - 1, player.GetComponent<CheckpointSystem>().currentCheckpoint.transform.position.y, player.GetComponent<CheckpointSystem>().currentCheckpoint.transform.position.z - 1);
+        BBoop.transform.position = new Vector3(BBoop.GetComponent<CheckpointSystem>().currentCheckpoint.transform.position.x + 2, BBoop.GetComponent<CheckpointSystem>().currentCheckpoint.transform.position.y, BBoop.GetComponent<CheckpointSystem>().currentCheckpoint.transform.position.z + 2);
+    }
 }
 
